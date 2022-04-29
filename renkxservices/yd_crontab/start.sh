b2a6cf2ccc48283bd96fbb0996df3e419f5e2189
liunian <m@renkx.com>
2022-04-30 04:42:41 +0800

    update

---
+++ renkxservices/yd_crontab/start.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -e
+
+cd /www/wwwroot
+
+echo
+echo "start time：`date '+%Y-%m-%d %H:%M:%S'`"
+
+echo 'cache initialize ...'
+#php artisan config:cache
+#php artisan view:cache
+#php artisan route:cache
+#php artisan event:cache
+
+#exec /usr/local/bin/php /www/wwwroot/artisan horizon --queue=default
+exec supercronic /etc/crontabs/www-data
