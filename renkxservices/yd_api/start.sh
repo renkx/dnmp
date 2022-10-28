d93b68e4417e1f261dfd6f1bc0eb06d28120d9e9
renkx <m@renkx.com>
2022-10-28 11:01:52 +0800

    新增node，chokidar文件监视库，用于octane自动重启

--- renkxservices/yd_api/start.sh
+++ renkxservices/yd_api/start.sh
@@ -7,6 +7,8 @@ cd /www/wwwroot
 echo
 echo "start time：`date '+%Y-%m-%d %H:%M:%S'`"
 
+cnpm install --save-dev chokidar
+
 http_workers=${HTTP_WORKERS:-4}
 
 task_workers=${TASK_WORKERS:-4}
@@ -20,5 +22,5 @@ echo 'cache initialize ...'
 #php artisan event:cache
 
 #exec php-fpm
-exec /usr/local/bin/php /www/wwwroot/artisan octane:start --host=0.0.0.0 --port=$http_port --workers=$http_workers --task-workers=$task_workers
+exec /usr/local/bin/php /www/wwwroot/artisan octane:start --watch --host=0.0.0.0 --port=$http_port --workers=$http_workers --task-workers=$task_workers
 
