96f5d03392d8b590410f6628e371bcef220a65ef
renkx <m@renkx.com>
2022-11-03 22:42:34 +0800

    update

--- renkxservices/yd_api/start.sh
+++ renkxservices/yd_api/start.sh
@@ -13,6 +13,8 @@ http_workers=${HTTP_WORKERS:-4}
 
 task_workers=${TASK_WORKERS:-4}
 
+max_requests=${MAX_REQUESTS:-500}
+
 http_port=${HTTP_PORT:-9000}
 
 echo 'cache initialize ...'
@@ -22,5 +24,5 @@ echo 'cache initialize ...'
 #php artisan event:cache
 
 #exec php-fpm
-exec /usr/local/bin/php /www/wwwroot/artisan octane:start --watch --host=0.0.0.0 --port=$http_port --workers=$http_workers --task-workers=$task_workers
+exec /usr/local/bin/php /www/wwwroot/artisan octane:start --watch --host=0.0.0.0 --port=$http_port --workers=$http_workers --task-workers=$task_workers --max-requests=$max_requests
 
