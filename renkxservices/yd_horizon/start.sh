da104bc5d34ead5549005110e082a7dbdb501c47
renkx <m@renkx.com>
2022-11-07 13:57:25 +0800

    update

--- renkxservices/yd_horizon/start.sh
+++ renkxservices/yd_horizon/start.sh
@@ -7,11 +7,21 @@ cd /www/wwwroot
 echo
 echo "start time：`date '+%Y-%m-%d %H:%M:%S'`"
 
+open_test=${OPEN_TEST}
+
+max_processes=${MAX_PROCESSES:-10}
+
 echo 'cache initialize ...'
 #php artisan config:cache
 #php artisan view:cache
 #php artisan route:cache
 #php artisan event:cache
 
-exec /usr/local/bin/php /www/wwwroot/artisan horizon --test
+if [[ "$open_test" = "true" ]]; then
+    exec /usr/local/bin/php /www/wwwroot/artisan horizon --test --maxProcesses=$max_processes
+else
+    exec /usr/local/bin/php /www/wwwroot/artisan horizon
+fi
+
+
 
